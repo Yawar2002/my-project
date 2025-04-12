4611507c6befe471fc81a9b2b2edda0e1ec39173
yawarali <yawaralibalti617@gmail.com>
2025-04-12 19:53:15 +0500

    How to make component

---
+++ components/CheckBox.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="border bg-red-500 w-1/2 h-screen text-white text-2xl">
+        {{ props.title }} <br>
+        {{ props.likes }}
+    </div>
+</template>
+<script setup>
+
+const props = defineProps({
+    title: String,
+    likes: Number
+})
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <CheckBox :title="'Alnafi we are here'" :likes="2" />
+        <CheckBox :title="'Hammas'" :likes="20" />
+    </div>
+</template>
